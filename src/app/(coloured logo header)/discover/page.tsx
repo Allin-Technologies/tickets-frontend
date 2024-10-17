4220e705786ceaf93112f2d466462059950d5326
kingsonseang <kingsonseang@gmail.com>
2024-10-17 10:03:14 +0100

    fix: discover page

--- src/app/(coloured logo header)/discover/page.tsx
+++ src/app/(coloured logo header)/discover/page.tsx
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { notFound } from "next/navigation";
+// import { notFound } from "next/navigation";
 import { api } from "@/lib/api";
 import { Discover } from "./client-side";
 
@@ -14,14 +14,14 @@ export default async function Page() {
     },
   });
 
-  if (request.response_code !== 200) {
-    notFound();
-  }
+  // if (request.response_code !== 200) {
+  //   notFound();
+  // }
 
   return (
     <main className='w-screen min-h-dvh'>
       <section className='w-full max-w-screen-2xl mx-auto px-4 pb-8 pt-24 md:px-6 md:pb-8 lg:px-8 xl:px-12 xl:pb-12 2xl:px-0 md:pt-26 lg:pt-32 xl:pt-36 2xl:pt-40 space-y-8 xl:space-y-16'>
-        <Discover initailData={request.data ?? []} />
+        <Discover initailData={request?.data ?? []} />
       </section>
     </main>
   );
