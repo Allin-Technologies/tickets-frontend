8a38fe03100c8319bd4735c26dc3ee18e9ca3d08
kingsonseang <kingsonseang@gmail.com>
2024-12-21 12:01:38 +0100

    modified:   src/actions/events/get.ts

--- src/actions/events/get.ts
+++ src/actions/events/get.ts
@@ -29,7 +29,9 @@ export async function getAllEvents(
   today.setHours(0, 0, 0, 0); // Set to start of today
 
   const filteredData = (request?.data ?? [])?.filter((event) => {
-    const eventDate = new Date(event.date.replace(/(\d+)(th|st|nd|rd)/, "$1"));
+    const eventDate = event?.date_ended
+      ? new Date(event.date_ended.replace(/(\d+)(th|st|nd|rd)/, "$1"))
+      : new Date(event.date.replace(/(\d+)(th|st|nd|rd)/, "$1"));
     eventDate.setHours(23, 59, 59, 999); // Set to end of event day
     return eventDate >= today;
   });
