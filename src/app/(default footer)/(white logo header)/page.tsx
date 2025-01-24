7a69d9a7a4453cbcf9ef46ea25b128f90b6b5f5c
WISDOM-WOKE22 <wisdomwokedev@gmail.com>
2025-01-24 06:25:58 +0100

    chore: updated UI

--- src/app/(default footer)/(white logo header)/page.tsx
+++ src/app/(default footer)/(white logo header)/page.tsx
@@ -158,7 +158,7 @@ export default function Home() {
             asChild
           >
             <Link href='/discover'>
-              <span>Discover events</span>
+              <span>See use cases</span>
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
@@ -196,7 +196,7 @@ export default function Home() {
             asChild
           >
             <Link href='/discover'>
-              <span>Discover events</span>
+              <span>See use cases</span>
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 fill='none'
